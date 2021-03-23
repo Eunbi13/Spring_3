b2b75cc335dc85c2eac3af527a91fb883539f0f2
ba-lim <78071935+ba-lim@users.noreply.github.com>
2021-03-23 15:17:28 +0900

    pm

--- src/main/java/com/iu/s3/member/MemberDAO.java
+++ src/main/java/com/iu/s3/member/MemberDAO.java
@@ -1,20 +1,17 @@
 package com.iu.s3.member;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-@Repository //
+@Repository
 public class MemberDAO {
 	@Autowired
 	private SqlSession sqlSession;
 	
-	private final String NAMESPACE = "com.iu.s3.member.memberDAO.";
+	private final String NAMESPACE = "com.iu.s3.member.MemberDAO.";
 	
 	public int memberJoin(MemberDTO memberDTO) throws Exception{
 		
