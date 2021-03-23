8ea72f153c08a5903b0c890233c1bdfeb03fc2f5
ba-lim <78071935+ba-lim@users.noreply.github.com>
2021-03-23 14:27:54 +0900

    pm

---
+++ src/main/java/com/iu/s3/member/MemberDAO.java
@@ -0,0 +1,50 @@
+package com.iu.s3.member;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository //
+public class MemberDAO {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	private final String NAMESPACE = "com.iu.s3.member.memberDAO."
+	
+	public int memberjoin(MemberDTO memberDTO) throws Exception{
+		
+		
+		return sqlSession.insert(null, memberDTO)
+	}
+	
+	
+
+	public MemberDTO memberlogin(MemberDTO memberDTO) throws Exception {
+		
+		String sql = "SELECT * FROM member WHERE id=? and pw=?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, memberDTO.getId());
+		st.setString(2, memberDTO.getPw());
+		
+		ResultSet rs = st.executeQuery();
+		
+		if (rs.next()) {
+			memberDTO.setEmail(rs.getString("email"));
+			memberDTO.setphone(rs.getString("phone"));
+			memberDTO.setName(rs.getString("name"));
+		}else {
+			memberDTO=null;
+		}
+		
+		rs.close();
+		st.close();
+		con.close();
+		
+		return memberDTO;
+	}
+}
